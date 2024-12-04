41cfa67c16497abb68ba27fa7c6a4566965f4bff
fabinholima <fabio.delima21@gmail.com>
2024-12-04 05:08:07 -0400

    custom file

---
+++ custom.el
@@ -0,0 +1,21 @@
+;; Custom
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" "a60b04e5c0fef30209f9576f04651938472b57cb1dae0375d80a53a78f515f69" default))
+ '(epg-pinentry-mode 'loopback nil nil "Customized with use-package epa")
+ '(package-selected-packages
+   '(treemacs-icons-dired spacemacs-theme treemacs-projectile treemacs major-mode-hydra which-key typescript-mode smart-mode-line rust-mode poet-theme org-bullets olivetti ob-typescript ob-rust exwm exec-path-from-shell emms debbugs dashboard bbdb auctex all-the-icons-dired))
+ '(world-clock-list
+   '(("Asia/Calcutta" "Bengaluru")
+     ("America/New_York" "New York")
+     ("America/Los_Angeles" "Seatle")) nil nil "Customized with use-package time"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
